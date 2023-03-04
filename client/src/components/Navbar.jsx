e22fbbee96bcb5ff6298ecd024ea50faa3ae0eb9
ngzuan <nvtuan181102@gmail.com>
2023-03-04 16:35:13 +0700

    commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-secondary">
+      <div className="bg-black text-white max-w-screen-xl mx-auto">Hello</div>
+      <img />
+    </div>
+  );
+};
+
+export default Navbar;

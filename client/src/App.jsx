045a816e10647be83b21cc0a9f584c1026d9d227
ngzuan <nvtuan181102@gmail.com>
2023-03-05 17:10:01 +0700

    setup router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import Navbar from "./components/Navbar";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    element: <Home />,
+    path: "home",
+  },
+  {
+    element: <Login />,
+    path: "login",
+  },
+  {
+    element: <Register />,
+    path: "register",
+  },
+]);
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }

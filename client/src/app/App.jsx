4e7c8e8bd0eed585e5b42703ef34bea982a891d5
ngzuan <nvtuan181102@gmail.com>
2023-03-05 17:49:36 +0700

    redux

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
   {
